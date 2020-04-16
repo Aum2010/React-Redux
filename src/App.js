7c03485b583cb2ed906031a3e6b0c1e9168018d7
Wasurat <Aumaumone2009@gmail.com>
2020-04-16 22:17:15 +0700

    ตัวอย่าง Redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useSelector } from 'react-redux'
 
-function App() {
+export default function App() {
+  const counter = useSelector(state =>state.counter)
+  const islogged = useSelector(state =>state.islogged)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h1>Counter {counter}</h1>
+        <h1>Islogged {islogged}</h1>
     </div>
-  );
+  )
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,55 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore  } from 'redux';
+import { Provider } from 'react-redux'
+import allReducer from './reducer';
+
+
+const store = createStore(allReducer ,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+// //store 
+// //action increment
+// const increment = () => {
+//   return {
+//     type : 'INCREMENT' 
+//   }
+// }
+// const decrement = () => {
+//   return {
+//     type : 'DECREMENT'
+//   }
+// }
+// //reducer
+// const counter = (state = 0 , action) => {
+//   switch(action.type){
+//     case 'INCREMENT' :
+//       return state + 1 ;
+//     case 'DECREMENT' :
+//       return state - 1 ;  
+//   }
+// } 
+
+
+// //display
+
+// store.subscribe(() => console.log(store.getState()))
+
+// //dispath
+
+// store.dispatch(increment())
+// store.dispatch(increment())
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store = {store}>
+
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+    
+
+  </Provider>,
+
   document.getElementById('root')
 );
 
